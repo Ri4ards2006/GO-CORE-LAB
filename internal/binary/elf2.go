8f92f4f44edc2a997e9fb2b4ca78d9d51673d32b
Richard Zuikov <astroricards@gmail.com>
2026-03-13 17:40:25 +0100

    making a repolished and a better elf 2.0 script

---
+++ internal/binary/elf2.go
@@ -0,0 +1,45 @@
+package binary
+
+import (
+
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+var elfMagic = []byte{0x7f, 'E', 'L', 'F'}
+
+
+// Konstante für die ELF-Header-Felder
+const (
+	class32 = uint8(1)
+	class64 = uint8(2)
+
+	dataLittle = uint8(1)
+	dataBig    = uint8(2) 
+// little endian = 1, big endian = 2
+)
+
+// Maschinen-Architekturen Mapping lesbar statt Hex-Werten
+
+var machineNames = map[uint16]string{
+	0x3E: "x86-64",
+	0x28: "ARM",
+	0xB7: "AArch64",
+	0x00: "No specific instruction set",
+	0x02: "SPARC",
+	0x03: "x86",
+	0x08: "MIPS",
+	0x14: "PowerPC",
+	0x16: "S390",
+	0x28: "ARM",
+	0x2A: "SuperH",
+	0x32: "IA-64",
+	0x3E: "x86-64",
+	0xB7: "AArch64",
+	0xF3: "RISC-V",
+	0xF7: "Berkeley Packet Filter",
+	0x101: "WDC 65C816",
+	0xFF00: "LoOS (Operating system-specific)",
+	0xFFFF: "HiOS (Operating system-specific)",
+}
